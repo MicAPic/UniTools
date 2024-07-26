b70c1bedb928739f721e4cc988e9b8a465a05019
Michael Pavlov <micha.pavlov@gmail.com>
2024-07-27 00:00:22 +0300

    Add more methods for rich text tags

---
+++ Runtime/Extensions/RichTextExtensions.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace UniTools.Extensions
+{
+    public static partial class StringExtensions
+    {
+        public static string ToBold(this string self)
+        {
+            return WrapInTag(self, "b", "b");
+        }
+
+        public static string ToItalic(this string self)
+        {
+            return WrapInTag(self, "i", "i");
+        }
+        
+        public static string ToStrikethrough(this string self)
+        {
+            return WrapInTag(self, "strikethrough", "strikethrough");
+        }
+        
+        public static string ToUnderlined(this string self)
+        {
+            return WrapInTag(self, "underline", "underline");
+        }
+        
+        public static string ToSubscript(this string self)
+        {
+            return WrapInTag(self, "sub", "sub");
+        }
+        
+        public static string ToSuperscript(this string self)
+        {
+            return WrapInTag(self, "sup", "sup");
+        }
+
+        public static string Highlight(this string self, string xmlColor = "yellow", bool toBold = true)
+        {
+            return WrapInTag(toBold ? self.ToBold() : self, $"color={xmlColor}", "color");
+        }
+
+
+        public static string Highlight(this string self, Color color, bool toBold = true)
+        {
+            return self.Highlight("#" + ColorUtility.ToHtmlStringRGB(color), toBold);
+        }
+
+        private static string WrapInTag(string self, string openTag, string closeTag)
+        {
+            if (string.IsNullOrEmpty(self))
+            {
+                Debug.LogWarning($"String is not defined. Can't wrap in tags: {openTag} - {closeTag}");
+                return self;
+            }
+
+            return $"<{openTag}>{self}</{closeTag}>";
+        }
+    }
+}
--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -1,43 +1,9 @@
 ﻿using System.Linq;
-using UnityEngine;
 
 namespace UniTools.Extensions
 {
-    public static class StringExtensions
+    public static partial class StringExtensions
     {
-        public static string ToBold(this string self)
-        {
-            return WrapInTag(self.ToString(), "b", "b");
-        }
-
-        public static string ToItalic(this string self)
-        {
-            return WrapInTag(self.ToString(), "i", "i");
-        }
-
-        public static string Highlight(this string self, string xmlColor = "yellow", bool toBold = true)
-        {
-            return WrapInTag(toBold ? self.ToBold() : self, $"color={xmlColor}", "color");
-        }
-
-
-        public static string Highlight(this string self, Color color, bool toBold = true)
-        {
-            return self.Highlight("#" + ColorUtility.ToHtmlStringRGB(color), toBold);
-        }
-
-        private static string WrapInTag(string self, string openTag, string closeTag)
-        {
-            if (string.IsNullOrEmpty(self))
-            {
-                Debug.LogWarning($"String is not defined. Can't wrap in tags: {openTag} - {closeTag}");
-                return self;
-            }
-
-            return $"<{openTag}>{self}</{closeTag}>";
-        }
-
-
         public static string Remove(this string self, params string[] remove)
         {
             return remove.Aggregate(self, (current, t) => current.Replace(t, string.Empty));
